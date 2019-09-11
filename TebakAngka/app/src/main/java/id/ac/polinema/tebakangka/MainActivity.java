fd3f3e3b4af4b18a10e40ef9808d442dac7152fd
abdadc <auri.culare@yahoo.com>
2019-09-11 15:53:57 +0700

    Membuat aplikasi tebak angka

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -8,29 +8,81 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.Random;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
 	//  TODO: deklarasikan variabel di sini
+	private EditText numberText;
+	private Button guessButton, resetButton;
+	int min = 1;
+	int max = 100;
+	int randomNumber = initRandomNumber();
+
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		// TODO: bind layout di sini
+
+		numberText = findViewById(R.id.number_input);
+		guessButton = findViewById(R.id.guess_button);
+		resetButton = findViewById(R.id.reset_button);
+
+		resetButton.setEnabled(false);
 	}
 
 	// TODO: generate angka random di sini
-	private void initRandomNumber() {
+	private int initRandomNumber()
+	{
+		int random = (int) (Math.random() * ((max - min) + 1)) + min;
+		return random;
 
 	}
 
-	public void handleGuess(View view) {
+	public void handleGuess(View view)
+	{
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		if(numberText.getText().toString().equals(""))
+		{
+			Toast.makeText(this, "Isilah angka tebakan anda !!", Toast.LENGTH_SHORT).show();
+		}
+		else
+			{
+			int guessNumber = Integer.parseInt(numberText.getText().toString());
+
+			if(guessNumber > max)
+			{
+				Toast.makeText(this, "Angka tebakan anda lebih dari 100 !!", Toast.LENGTH_SHORT).show();
+			}
+			else if(guessNumber < min)
+			{
+				Toast.makeText(this, "Angka tebakan anda kurang dari 1 !!", Toast.LENGTH_SHORT).show();
+			}
+			else if(guessNumber > randomNumber)
+			{
+				Toast.makeText(this, "Angka tebakan anda terlalu besar !!", Toast.LENGTH_SHORT).show();
+			}
+			else if(guessNumber < randomNumber)
+			{
+				Toast.makeText(this, "Angka tebakan anda terlalu kecil !!", Toast.LENGTH_SHORT).show();
+			}
+			else{
+				Toast.makeText(this, "Angka tebakan anda benar, selamat !!", Toast.LENGTH_SHORT).show();
+				guessButton.setEnabled(false);
+				numberText.setEnabled(false);
+				resetButton.setEnabled(true);
+			}
+		}
 	}
 
-	public void handleReset(View view) {
+	public void handleReset(View view)
+	{
 		// TODO: Reset tampilan
+		numberText.setText("");
+		guessButton.setEnabled(true);
+		numberText.setEnabled(true);
+		randomNumber = initRandomNumber();
+		resetButton.setEnabled(false);
 	}
 }
